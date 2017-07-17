b98fbd28af09bcd0d0bed9137ccbce9be74426e3
Varabe <zertarx@gmail.com>
2017-07-17 03:01:05 +0200

    [add] threading events, which allows for parallel execution of several commands and non-stop longPolling

--- mainloop.py
+++ mainloop.py
@@ -1,8 +1,10 @@
+from threading import Thread, Event, Lock, active_count
 from logging import getLogger
 from requests import post
 
+from lib.commands.errors import UserExit
+from lib.errors import sendErrorMessage
 from lib.messages import iterMessages
-from lib.errors import ErrorManager
 from lib.vk import getResponseDict
 from lib.utils import vk
 
@@ -12,8 +14,10 @@ logger = getLogger("bot.main")
 
 def main():
 	server = LongPollServer()
+	manager = ThreadManager(max_count=5)
 	logger.debug("Starting longPolling...")
-	server.mainLoop()
+	Thread(target=manager.eventCheckingLoop).start()
+	Thread(target=manager.longPollingLoop, args=(server, ), daemon=True).start()
 
 
 class LongPollServer:
@@ -38,23 +42,54 @@ class LongPollServer:
 				"ts":self.ts, "wait":wait_time,
 				"version":version}
 
-	def mainLoop(self):
-		while True:
-			response = post(self.url, self.kwargs)
+
+class ThreadManager:
+	def __init__(self, max_count=3):
+		must_have_threads_count = 3
+		self.max_threads = max_count + must_have_threads_count
+		self.event_lock = Lock()
+		self.new_event = Event()
+		self.exception = None
+		self.events = []
+
+	def eventCheckingLoop(self):
+		while self.exception is None:
+			while self.events:
+				if active_count() < self.max_threads:
+					with self.event_lock:
+						event = self.events.pop(0)
+						if "updates" in event:
+							event_thread = Thread(
+								target=self.handleEvent,
+								args=(event['updates'],),
+								daemon=True)
+							event_thread.start()
+			self.new_event.clear()
+			self.new_event.wait()
+		self.finish()
+
+	def longPollingLoop(self, server):
+		while self.exception is None:
+			response = post(server.url, server.kwargs)
 			response = getResponseDict(response)
-			self.kwargs['ts'] = response['ts']
-			# with TEST_LOCK:
-			# event_list.append(response)
-			handle(response)
+			server.kwargs['ts'] = response['ts']
+			self.events.append(response)
+			self.new_event.set()
 
+	def handleEvent(self, updates):
+		messages = iterMessages(updates)
+		try:
+			for message in messages:
+				message.handle()
+		except Exception as e:
+			self.exception = e
+			self.new_event.set()
 
-def handle(response):
-	if "updates" in response:
-		message_list = iterMessages(response['updates'])
-		for message in message_list:
-			message.handle()
+	def finish(self):
+		if type(self.exception) != UserExit:
+			sendErrorMessage("LongPolling", self.exception)
+		logger.debug("EventLoop finished")
 
 
 if __name__ == "__main__":
-	with ErrorManager("MultiThread bot"):
-		main()
+	main()
