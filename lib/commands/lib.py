c10c724a92c56acb10f96dc4beed5620e850a92d
Varabe <zertarx@gmail.com>
2017-07-19 21:40:43 +0200

    [fix] tried to use int to split string

--- lib/commands/lib.py
+++ lib/commands/lib.py
@@ -41,8 +41,7 @@ class Variable(str):
 
 
 def getAssignmentArgs(text):
-	equal_sign = text.index("=")
-	name, value = text.split(equal_sign, maxsplit=1)
+	name, value = text.split("=", maxsplit=1)
 	value = extractArgs(value, argcount=1)[0]
 	return name, value
 
