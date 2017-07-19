77132abe534e0d62c51f4d6c919d31858d318859
Varabe <zertarx@gmail.com>
2017-07-19 17:20:50 +0200

    [add] ResponseError and 'no internet' error

--- lib/vk.py
+++ lib/vk.py
@@ -1,3 +1,4 @@
+from requests.exceptions import SSLError
 from requests import post
 from time import sleep
 import json
@@ -20,7 +21,12 @@ class Vk:
 
 	def makeRequest(self, method, **kwargs):
 		sleep(sleep_time)
-		return getResponseDict(post(method, kwargs))
+		try:
+			response = post(method, kwargs)
+		except SSLError as e:
+			raise VkError("No internet connection.") from e
+		else:
+			return getResponseDict(response)
 
 
 def getResponseDict(response):
@@ -28,16 +34,9 @@ def getResponseDict(response):
 	if "response" in json_dict:
 		return json_dict['response']
 	elif "error" in json_dict:
-		raise VkError(json_dict['error'])
+		raise ResponseError(json_dict['error'])
 	else:
 		return json_dict
-
-
-class VkError(Exception):
-	def __init__(self, error):
-		message = "\nMessage: {}\nParameters: {}".format(
-			error['error_msg'], error['request_params'])
-		super().__init__(message)
 
 
 class LongPollServer:
@@ -62,3 +61,15 @@ class LongPollServer:
 		response = getResponseDict(response)
 		self.kwargs['ts'] = response['ts']
 		return response
+
+
+class VkError(Exception):
+	def __init__(self, message):
+		super().__init__(message)
+
+
+class ResponseError(VkError):
+	def __init__(self, error_dict):
+		message = "\nMessage: {}\nParameters: {}".format(
+			error_dict['error_msg'], error_dict['request_params'])
+		super().__init__(message)
